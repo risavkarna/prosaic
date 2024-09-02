761c4d34ed062ddfcdab1e37b5287f415ee94fc8
Risav Karna <git@risav.dev>
2024-09-02 05:51:56 -0400

    build(init): add a barebones angular editor

--- apps/prism/src/app/app.component.ts
+++ apps/prism/src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { NgxTiptapModule } from 'ngx-tiptap';
+import { Editor } from '@tiptap/core';
+import StarterKit from '@tiptap/starter-kit';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [RouterModule, NgxTiptapModule, FormsModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'prism';
+
+  editor = new Editor({
+    extensions: [StarterKit],
+  });
+
+  value = '<p>Hello, Tiptap!</p>'; // can be HTML or JSON, see https://www.tiptap.dev/api/editor#content
+
+  ngOnDestroy(): void {
+    this.editor.destroy();
+  }
 }
